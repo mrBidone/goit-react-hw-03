7e04240221dfd22e53f8f7096a1bcb02ed169683
mrBidone <ab.bz@ukr.net>
2024-08-20 00:16:47 +0200

    Add formik form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,64 +1,39 @@
 import { Formik, Form, Field } from "formik";
 
+const INITIAL_VALUES = {
+  contactName: "",
+  contactNumber: "",
+};
+
 const ContactForm = ({ onAddContact }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const formElements = e.currentTarget.elements;
-
-    const name = formElements.contactName.value;
-    const number = formElements.contactNumber.value;
-
+  const handleSubmit = (values, actions) => {
     const contactObject = {
-      name,
-      number,
+      name: values.contactName,
+      number: values.contactNumber,
     };
 
     onAddContact(contactObject);
 
-    e.currentTarget.reset();
+    actions.resetForm();
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit} action="">
-        <label htmlFor="">
-          Name
-          <input type="text" name="contactName" required />
-        </label>
-        <label htmlFor="">
-          Number
-          <input type="text" name="contactNumber" required />
-        </label>
-        <button type="submit">Add contact</button>
-      </form>
+      <Formik initialValues={{ INITIAL_VALUES }} onSubmit={handleSubmit}>
+        <Form>
+          <label htmlFor="">
+            Name
+            <Field type="text" name="contactName" required />
+          </label>
+          <label htmlFor="">
+            Number
+            <Field type="text" name="contactNumber" required />
+          </label>
+          <button type="submit">Add contact</button>
+        </Form>
+      </Formik>
     </>
   );
 };
-
-// const INITIAL_VALUES = {
-//   contactName: "",
-//   contactNumber: "",
-// };
-
-// const ContactForm = ({ onAddContact }) => {
-//   return (
-//     <Formik initialValues={INITIAL_VALUES} onSubmit={(e) => {
-
-//     }}>
-//       <Form>
-//         <label>
-//           <span>Name</span>
-//           <Field type="text" name="contactName" />
-//         </label>
-//         <label>
-//           <span>Number</span>
-//           <Field type="text" name="contactNumber" />
-//         </label>
-//         <button type="submit">Add contact</button>
-//       </Form>
-//     </Formik>
-//   );
-// };
 
 export default ContactForm;
